810533f470e4306674e2b901205240be4fd09882
Zeel <zeelp741@gmail.com>
2021-05-14 02:15:14 -0400

    added packages

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+// Connect react file to index HTML
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from "./App"
+
+// Connecting to div with an Id of root 
+ReactDOM.render(<App />, document.getElementById('root'))
