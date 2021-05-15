d2a52ede464f0d57203532f2ce6f28f48b91b649
Zeel <zeelp741@gmail.com>
2021-05-14 23:09:17 -0400

    updated JSX structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import icon from "./images/icon.png"
 const App = () =>{
     return(
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxwidth = 'lg'>
+            <AppBar position = "static" color = "inherit">
+                <Typography varaint = "h2" align = "center">Memories</Typography>
+                <img src = {icon} alt = "memories" height = "60" />
+            </AppBar>
+
+            <Grow in>
+                <Grid container justify = "space-between" alignItems = "stretch" spacing = {3}>
+                    <Grid item xs = {12} sm = {7}>
+                        <Posts/>
+                    </Grid>
+                    <Grid item xs = {12} sm = {7}>
+                        <Form/>
+                    </Grid>
+                </Grid>
+            </Grow>
+
+        </Container>
     );
 }
 
