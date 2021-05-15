ccb4a66625eb2a5ec4fbd201b5266877b9b9a614
Zeel <zeelp741@gmail.com>
2021-05-15 04:41:41 -0400

    most basic MERN stack complete, looks ugly

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -6,9 +6,9 @@ export default (posts = [], action) => {
 
     switch(action.type){
         case 'FETCH_ALL':
-            return posts
+            return action.payload
         case 'CREATE':
-            return posts
+            return [...posts, action.payload]
         default:
             return posts
     }
