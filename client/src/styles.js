3cc740ff7e4734dd7b8e6b6cd05649bdaf075f7f
Zeel <zeelp741@gmail.com>
2021-05-19 03:14:39 -0400

    update

--- client/src/styles.js
+++ client/src/styles.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
   appBar: {
-    borderRadius: 15,
+    borderRadius: 50,
     margin: '30px 0',
     display: 'flex',
     flexDirection: 'row',
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from "cors"
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 // Initalize App
@@ -18,14 +18,21 @@ app.use(cors())
 
 // Connects to application
 // Every route in postRoutes is going to start with posts
-app.use('/posts', postRoutes)
+app.use('/posts', postRoutes) 
+
+app.get('/', (req, res) => {
+  res.send("Hello to Post it API")
+})
 
 // Connect to database
 const CONNECTION_URL = 'mongodb+srv://post-it-admin:post-it-admin-password@cluster0.hpkpu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
+
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false); 
+// app.listen(PORT);
+
+mongoose.set('useFindAndModify', false);  
